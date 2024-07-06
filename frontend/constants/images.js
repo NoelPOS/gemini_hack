30bdcc3d4fea454206b7befe44036c38033a3569
Lucassan4225 <145424551+Lucassan4225@users.noreply.github.com>
2024-07-06 23:16:31 +0700

    readGen

--- frontend/constants/images.js
+++ frontend/constants/images.js
@@ -1,4 +1,6 @@
 import writingrobot1 from "../assets/images/writing_robot1.svg";
+import robot_reading from "../assets/images/robot_reading 4.svg"
 export default {
   writingrobot1,
+  robot_reading,
 };
--- frontend/src/app/components/Reading/ReadingGenerate.tsx
+++ frontend/src/app/components/Reading/ReadingGenerate.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import images from "../../../../constants/images";
+import Image from "next/image";
 
 type Props = {};
 
 const ReadingGenerate = (props: Props) => {
   return (
     <div className="max-w-[1260px] h-96 w-full items-center justify-center">
-      <div className="flex-none w-3/5 h-2/4">
-        <img
-          src="path_to_your_image.jpg"
-          alt="Description of image"
-          className="w-full h-full object-cover"
-        />
+      <div className="flex-none w-1/5 h-2/4 ml-40">
+        <Image 
+        src={images.robot_reading} 
+        alt="robot"
+        className="w-[320px] h-[510px]" />
       </div>
     </div>
   );
