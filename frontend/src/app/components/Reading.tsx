04607d9df6961ef8f13a89455fdd401eafdd4f7a
Lucassan4225 <145424551+Lucassan4225@users.noreply.github.com>
2024-07-06 22:09:57 +0700

    main

--- frontend/src/app/components/Reading.tsx
+++ frontend/src/app/components/Reading.tsx
@@ -6,13 +6,30 @@ type Props = {};
 
 function Reading({}: Props) {
   return (
-    <div className="text-slate-800 flex">
-      <div className="flex flex-col w-2/5">
-        <Image src={images.writingrobot1} alt="robot" />
+    <div className="w-screen bg-purple-500 h-96 flex">
+      <div className="flex-none w-3/5 h-2/4">
+        <img
+          src="path_to_your_image.jpg"
+          alt="Description of image"
+          className="w-full h-full object-cover"
+        />
       </div>
-      <div className="flex"></div>
+      <div className="flex-grow text-slate-800 p-4 h-2/4">
+        Reading
+      </div>
+      <div className="text-slate-800 flex">
+        <div className="flex flex-col w-2/5">
+          <Image src={images.writingrobot1} alt="robot" />
+        </div>
+        <div className="flex"></div>
+      </div>
     </div>
+    
   );
+  
+    
+  
 }
+
 
 export default Reading;
