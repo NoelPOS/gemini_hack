79c7ceff3d8f986a23de46a5af85cf9686acc97e
Kyaw Ye Lwin@Anmol <141581004+MarioK404@users.noreply.github.com>
2024-07-07 00:19:31 +0700

    Question Component-Writing

--- frontend/src/app/components/Writing.tsx
+++ frontend/src/app/components/Writing.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import WordList from "./Writing/WordList";
+import Question from "./Writing/Question";
+
 type Props = {};
 
 function Writing({}: Props) {
   return (
-    <div className="text-slate-800">
-      Writing
-      <WordList />
+    <div className="flex h-screen p-4">
+      <div className="flex-grow flex items-start space-x-8">
+        <WordList />
+        <Question />
+      </div>
     </div>
   );
 }
